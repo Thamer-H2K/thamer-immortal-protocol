6ebe4d83ccc9fd70977b2b9fd9e73d676314fc6e
Thamer-H2K <122726877+Thamer-H2K@users.noreply.github.com>
2025-11-15 12:05:05 +0300

    🧪 Add tests/__init__.py - Initialize test suite

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+"""
+Thamer Immortal Protocol - Test Suite
+Copyright © 2025 Thamer-H2K. All Rights Reserved.
+Contact: frankly.sa@gmail.com
+"""
+
+__version__ = "1.0.0"
+__author__ = "Thamer Aljadaan (CyberX)"
